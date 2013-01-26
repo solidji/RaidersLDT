eff8dc1f5360bff9d6ad94d002164152f3825e53
计 炜 <solidji@jimatoMacBook-Air.local>
2013-01-26 19:45:55 +0800

    Initial Commit

---
+++ RaidersIMMT/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RaidersIMMT
+//
+//  Created by 计 炜 on 13-1-26.
+//  Copyright (c) 2013年 计 炜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
